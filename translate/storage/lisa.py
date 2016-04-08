1efc4bf2ccfcffb507112244811039dd9e414bd3
Matjaž Horvat <matjaz.horvat@gmail.com>
2016-04-08 15:08:18 +0200

    Fix indent in TMX, TBX, XLIFF output file

--- translate/storage/lisa.py
+++ translate/storage/lisa.py
@@ -324,8 +324,25 @@ class LISAfile(base.TranslationStore):
         if new:
             self.body.append(unit.xmlelement)
 
+    # Source: http://effbot.org/zone/element-lib.htm#prettyprint
+    def indent(self, elem, level=0):
+        i = "\n" + level*"  "
+        if len(elem):
+            if not elem.text or not elem.text.strip():
+                elem.text = i + "  "
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+            for elem in elem:
+                self.indent(elem, level+1)
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+        else:
+            if level and (not elem.tail or not elem.tail.strip()):
+                elem.tail = i
+
     def __str__(self):
         """Converts to a string containing the file's XML"""
+        self.indent(self.document.getroot())
         return etree.tostring(self.document, pretty_print=True,
                               xml_declaration=True, encoding='utf-8')
 
